2ca3d171ea3e195227d23624a83b42e89347501b
Henrik <henrik@stud.ntnu.no>
2021-12-22 01:09:07 +0100

    stonks?

--- rust-stonk/src/main.rs
+++ rust-stonk/src/main.rs
@@ -1,3 +1,16 @@
+use yahoo_finance_api as yahoo;
+use std::time::{Duration, UNIX_EPOCH};
+use chrono::prelude::*;
+use tokio_test;
+
 fn main() {
-    println!("Hello, world!");
+    let provider = yahoo::YahooConnector::new();
+    // get the latest quotes in 1 minute intervals
+    let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1m")).unwrap();
+    // extract just the latest valid quote summery
+    // including timestamp,open,close,high,low,volume
+    let quote = response.last_quote().unwrap();
+    let time: DateTime<Utc> =
+        DateTime::from(UNIX_EPOCH + Duration::from_secs(quote.timestamp));
+    println!("At {} quote price of Apple was {}", time.to_rfc3339(), quote.close);
 }
