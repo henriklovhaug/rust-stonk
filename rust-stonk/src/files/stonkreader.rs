38ceb1620bcedd66c6a44e54e67b3593c80eba1c
Henrik <henrik@stud.ntnu.no>
2021-12-23 21:46:01 +0100

    implemented reader json

--- rust-stonk/src/files/stonkreader.rs
+++ rust-stonk/src/files/stonkreader.rs
@@ -1,1 +1,12 @@
+use std::{fs::File, io::Read};
 
+use crate::datatypes::stonk::Stonk;
+
+#[allow(dead_code)]
+pub fn get_stonk_from_file() -> Vec<Stonk> {
+    let mut file = File::open("stonks.json").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let stonks: Vec<Stonk> = serde_json::from_str(&contents).unwrap();
+    stonks
+}
