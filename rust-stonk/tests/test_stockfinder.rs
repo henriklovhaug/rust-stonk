34c38a73531a43defa940180c7031b2896cc67c7
Henrik <henrik@stud.ntnu.no>
2021-12-26 22:49:17 +0100

    Removed deprecated code

--- rust-stonk/tests/test_stockfinder.rs
+++ rust-stonk/tests/test_stockfinder.rs
@@ -1,12 +1,7 @@
-extern crate rust_stonk;
+use rust_stonk::stonk_finder::stonk_finder::get_latest_stonk;
 
-#[cfg(test)]
-mod test_finder {
-    use rust_stonk::stonk_finder::stonk_finder::get_latest_stonk;
-
-    #[tokio::test]
-    async fn test_find_stonk() {
-        let stonks = get_latest_stonk("AAPL").await.unwrap();
-        assert_ne!(stonks.len(), 0);
-    }
+#[tokio::test]
+async fn test_find_stonk() {
+    let stonks = get_latest_stonk("AAPL").await.unwrap();
+    assert_ne!(stonks.len(), 0);
 }
